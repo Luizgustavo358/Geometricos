d82a1b97e71b1271d3abc5c5662c73f4847659f3
Luizgustavo358 <luiz.gustavo358@gmail.com>
2017-07-31 12:07:11 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# geometricos
+
+**Feito por**: Luiz Gustavo
+
+Sobre
+---
+- Objetivo: Faz a soma dos lados dos geometricos e faz a area.
+- Uso de várias classes.
+- Código feito em Python.
+
+Para Rodar o Programa:
+---
+Escreva no Terminal do Linux:
+
+```sh
+$python geometricos.py
+```
+
+O que usei:
+---
+- [PyCharm Edu](https://www.jetbrains.com/pycharm-edu/download/#section=windows) - Para Escrever o código e rodá-lo.
+
+Enjoy it :+1:
---
+++ geometricos.py
@@ -0,0 +1,44 @@
+'''
+    Nome do Programa: area_de_geometricos.py
+    Autor: Luiz Gustavo Bragança dos Santos
+    Objetivo: Faz a soma dos lados dos geometricos e faz a area.
+'''
+
+''' Classe Triangulo '''
+class Triangulo(object):
+
+    # definir dados
+    base = None
+    altura = None
+    lado1 = None
+    lado2 = None
+
+    def perimetro(self, lado1, lado2, base):
+        # somando lados
+        result = lado1 + lado2 + base
+        return result
+    # end perimetro
+# end class Triangulo
+
+''' Classe Triangulo '''
+class Quadrado(object):
+    # definir dados
+# end class Quadrado
+
+''' Classe Triangulo '''
+class Retangulo(object):
+    # definir dados
+# end class Retangulo
+
+''' Classe Triangulo '''
+class Trapezio(object):
+    # definir dados
+# end class Trapezio
+
+def apresentacao():
+    print("")
+# end apresentacao()
+
+if __name__ == '__main__':
+
+# end main
