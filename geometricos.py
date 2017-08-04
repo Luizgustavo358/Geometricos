1ffbb966d1e98eec008abdc6a4a4fbee1cd9f530
Luizgustavo358 <luiz.gustavo358@gmail.com>
2017-08-03 22:26:31 -0300

    adicionado métodos de calcular o perímetro e área dos geometricos

--- geometricos.py
+++ geometricos.py
@@ -4,6 +4,9 @@
     Objetivo: Faz a soma dos lados dos geometricos e faz a area.
 '''
 
+# importando Classe
+import math
+
 ''' Classe Triangulo '''
 class Triangulo(object):
 
@@ -69,28 +72,83 @@ class Retangulo(object):
 class Trapezio(object):
 
     # definir dados
+    base_maior = None
+    base_menor = None
+    altura = None
 
+    def perimetro_trapezio(self, base_maior, base_menor, lado1, lado2):
+        # achando o perimetro do Trapezio
+        perimetro = base_maior + base_menor + lado1 + lado2
+        return perimetro
+    # end perimetro_trapezio()
+
+    def area_trapezio(self, base_menor, base_maior, altura):
+        # achando a area do Trapezio
+        area = ((base_menor + base_maior) * altura) / 2
+        return area
+    # end area_trapezio()
 # end class Trapezio
 
 ''' Classe Circulo '''
 class Circulo(object):
 
     # definir dados
+    raio = None
 
+    def perimetro_circulo(self, raio):
+        # achando o perimetro do Circulo
+        perimetro = 2 * math.pi + raio
+        return perimetro
+    # end perimetro_circulo()
+
+    def area_circulo(self, raio):
+        # achando a area do Circulo
+        area = math.pi * (raio ** 2)
+        return area
+    # end area_circulo()
 # end class Circulo
 
 ''' Classe Losango '''
 class Losango(object):
 
     # definir dados
+    lado = None
+    diagonal_maior = None
+    diagonal_menor = None
 
+    def perimetro_losango(self, lado):
+        # achando o perimetro do Losango
+        perimetro = 4 * lado
+        return perimetro
+    # end perimetro_losango()
+
+    def area_losango(self):
+        # achando a area do Losango
+        area = (diagonal_maior * diagonal_menor) / 2
+        return area
+    # end area_losango()
 # end class Losango
 
 ''' Classe Paralelogramo '''
 class Paralelogramo(object):
 
     # definir dados
+    lado_a = None
+    lado_b = None
+    base = None
+    altura = None
 
+    def perimetro_paralelogramo(self, lado_a, lado_b):
+        # achando o perimetro do Paralelogramo
+        perimetro = 2 * (lado_a + lado_b)
+        return perimetro
+    # end perimetro_paralelogramo()
+
+    def area_paralelogramo(self):
+        # achando area do Paralelogramo
+        area = base * altura
+        return area
+    # end area_paralelogramo()
 # end class Paralelogramo
 
 # -------------------------------- Aqui comeca o programa
