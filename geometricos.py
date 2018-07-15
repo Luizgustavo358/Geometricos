748674da01b5a6069ec13a12d361ec8caec29a38
Luizgustavo358 <luiz.braganca@sga.pucminas.br>
2018-07-15 16:02:51 -0300

    comentarios atualizados

--- geometricos.py
+++ geometricos.py
@@ -1,7 +1,7 @@
 '''
     Nome do Programa: geometricos.py
     Autor: Luiz Gustavo Bragança dos Santos
-    Objetivo: Faz a soma dos lados dos geometricos e faz a area.
+    Objetivo: Calcula a Area e Perimetro de um Geometrico.
 '''
 
 # importando Classe
@@ -11,13 +11,23 @@ import math
 ''' Classe Triangulo '''
 class Triangulo(object):
 
+    #
+    # Metodo que calcula o perimetro de um triangulo.
+    # @param int lado1, lado2, base -> passados pelo usuario.
+    # @return int result -> retorna o perimetro.
+    #
     def perimetro_triangulo(self, lado1, lado2, base):
         # somando lados
-        result = lado1 + lado2 + base
-        return result
+        perimetro = lado1 + lado2 + base
+        return perimetro
     # end perimetro_triangulo()
 
 
+    #
+    # Metodo que calcula a area de um triangulo.
+    # @param int base, altura -> passados pelo usuario.
+    # @return int area -> retorna a area.
+    #
     def area_triangulo(self, base, altura):
         # calculando a area
         area = (base * altura) / 2
@@ -29,15 +39,9 @@ class Triangulo(object):
 ''' Classe Quadrado '''
 class Quadrado(object):
 
-    # definir dados
-    lado = None
-
-
-    def __init__(lado):
-        self.lado = lado
-    # end construtor
-
-
+    #
+    #
+    #
     def perimetro_quadrado(self, lado):
         # somando lados
         perimetro = 4 * lado
@@ -45,6 +49,9 @@ class Quadrado(object):
     # end perimetro_quadrado()
 
 
+    #
+    #
+    #
     def area_quadrado(self, lado):
         # achando a area
         area = lado ** 2
@@ -56,17 +63,9 @@ class Quadrado(object):
 ''' Classe Retangulo '''
 class Retangulo(object):
 
-    # definir dados
-    base = None
-    altura = None
-
-
-    def __init__(base, altura):
-        self.base = base
-        self.altura = altura
-    # end construtor
-
-
+    #
+    #
+    #
     def perimetro_retangulo(self, base, altura):
         # fazendo a soma dos lados
         perimetro = 2 * (base + altura)
@@ -74,6 +73,9 @@ class Retangulo(object):
     # end perimetro_retangulo()
 
 
+    #
+    #
+    #
     def area_retangulo(self, base, altura):
         # achando a area
         area = base * altura
@@ -84,26 +86,19 @@ class Retangulo(object):
 ''' Classe Trapezio '''
 class Trapezio(object):
 
-    # definir dados
-    base_maior = None
-    base_menor = None
-    altura = None
-
-
-    def __init__(base_maior, base_menor, altura):
-        self.base_maior = base_maior
-        self.base_menor = base_menor
-        self.altura = altura
-    # end construtor
-
-
+    #
+    #
+    #
     def perimetro_trapezio(self, base_maior, base_menor, lado1, lado2):
         # achando o perimetro do Trapezio
         perimetro = base_maior + base_menor + lado1 + lado2
         return perimetro
     # end perimetro_trapezio()
 
-
+    
+    #
+    #
+    #
     def area_trapezio(self, base_menor, base_maior, altura):
         # achando a area do Trapezio
         area = ((base_menor + base_maior) * altura) / 2
@@ -114,15 +109,9 @@ class Trapezio(object):
 ''' Classe Circulo '''
 class Circulo(object):
 
-    # definir dados
-    raio = None
-
-
-    def __init__(lado):
-        self.lado = lado
-    # end construtor
-
-
+    #
+    #
+    #
     def perimetro_circulo(self, raio):
         # achando o perimetro do Circulo
         perimetro = 2 * math.pi + raio
@@ -130,6 +119,9 @@ class Circulo(object):
     # end perimetro_circulo()
 
 
+    #
+    #
+    #
     def area_circulo(self, raio):
         # achando a area do Circulo
         area = math.pi * (raio ** 2)
@@ -141,19 +133,9 @@ class Circulo(object):
 ''' Classe Losango '''
 class Losango(object):
 
-    # definir dados
-    lado = None
-    diagonal_maior = None
-    diagonal_menor = None
-
-
-    def __init__(lado, diagonal_maior, diagonal_menor):
-        self.lado = lado
-        self.diagonal_maior = diagonal_maior
-        self.diagonal_menor = diagonal_menor
-    # end construtor
-
-
+    #
+    #
+    #
     def perimetro_losango(self, lado):
         # achando o perimetro do Losango
         perimetro = 4 * lado
@@ -161,6 +143,9 @@ class Losango(object):
     # end perimetro_losango()
 
 
+    #
+    #
+    #
     def area_losango(self):
         # achando a area do Losango
         area = (diagonal_maior * diagonal_menor) / 2
@@ -171,21 +156,9 @@ class Losango(object):
 ''' Classe Paralelogramo '''
 class Paralelogramo(object):
 
-    # definir dados
-    lado_a = None
-    lado_b = None
-    base = None
-    altura = None
-
-
-    def __init__(lado_a, lado_b, base, altura):
-        self.lado_a = lado_a
-        self.lado_b = lado_b
-        self.base = base
-        self.altura = altura
-    # end construtor
-
-
+    #
+    #
+    #
     def perimetro_paralelogramo(self, lado_a, lado_b):
         # achando o perimetro do Paralelogramo
         perimetro = 2 * (lado_a + lado_b)
@@ -193,6 +166,9 @@ class Paralelogramo(object):
     # end perimetro_paralelogramo()
 
 
+    #
+    #
+    #
     def area_paralelogramo(self):
         # achando area do Paralelogramo
         area = base * altura
@@ -204,6 +180,9 @@ class Paralelogramo(object):
 # -------------------------------- Aqui comeca o programa
 
 
+#
+# Metodo de apresentacao.
+#
 def apresentacao():
     print("\n+----------------------------------------------+")
     print("|            Programa Feito em Python          |")
@@ -214,6 +193,9 @@ def apresentacao():
 # end apresentacao()
 
 
+#
+# Metodo que mostra as opcoes.
+#
 def opcoes():
     print("\n0 - Sair")
     print("1 - Triangulo")
@@ -226,12 +208,20 @@ def opcoes():
 # end opcoes()
 
 
+#
+# Metodo que le as opcoes.
+# @retun int opcao -> numero escolhido.
+#
 def le_opcao():
     opcao = int(input("\nEntre com um número: "))
     return opcao
 # end le_opcao()
 
 
+#
+# Metodo escolhe a opcao.
+# @param int opcao -> escolhida pelo usuario.
+#
 def escolhe_opcao(opcao):
     if opcao == 1:
         funcTriangulo()
@@ -251,23 +241,70 @@ def escolhe_opcao(opcao):
 # end escolhe_opcao()
 
 
+#
+# Metodo que le a base de um geometrico.
+# @return int base -> escolhida pelo usuario.
+#
+def le_base():
+    base = int(input("\nQual a base: "))
+    return base
+# end le_base
+
+
+#
+# Metodo que le a Altura de um geometrico.
+# @return int altura -> escolhida pelo usuario.
+#
+def le_altura():
+    altura = int(input("\nQual é a altura: "))
+    return altura
+# end le_altura
+
+
+#
+# Metodo que le o lado de um geometrico.
+# @return int lado -> escolhida pelo usuario.
+#
+def le_lado():
+    lado = int(input("\nQual é o tamanho do lado: "))
+    return lado
+# end le_lado()
+
+
+#
+# Metodo que mostra as opcoes de calculo.
+#
+def pergunta():
+    print("\n0 - Nada")
+    print("1 - Perímetro")
+    print("2 - Área")
+# end pergunta()
+
+
+#
+# Metodo para o triangulo, onde calcula e mostra o perimetro ou area.
+#
 def funcTriangulo():
     print("-------------------------------------------------")
     print("\nVocê escolheu: Triangulo")
 
+    # instancia o objeto triangulo
+    triangulo = Triangulo()
+
+    # mostra as opcoes de calculo
     pergunta()
 
+    # escolhe a opcao
     tipo = int(input("\nQual tipo deseja: "))
 
+    # testa as opcoes
     if tipo == 1:
         # perimetro do triangulo
         base = le_base()
         lado1 = le_lado()
         lado2 = le_lado()
 
-        triangulo = Triangulo()
-
-        # calcula
+        # calcula o perimetro
         perimetro = triangulo.perimetro_triangulo(lado1, lado2, base)
 
         # mostra o perimetro
@@ -278,10 +315,10 @@ def funcTriangulo():
         base = le_base()
         altura = le_altura()
 
-        # calcula
+        # calcula a area
         area = triangulo.area_triangulo(base, altura)
 
-        # mostra o perimetro
+        # mostra a area
         print("\nA Área do Triângulo é de: ", area)
     # end if
 
@@ -289,60 +326,57 @@ def funcTriangulo():
 # end funcTriangulo
 
 
-def le_base():
-    base = int(input("\nQual a base: "))
-    return base
-# end le_base
-
-def le_altura():
-    altura = int(input("\nQual é a altura: "))
-    return altura
-# end le_altura
-
-
-def le_lado():
-    lado = int(input("\nQual é o tamanho do lado: "))
-    return lado
-# end le_lado()
-
-
-def pergunta():
-    print("\n0 - Nada")
-    print("1 - Perímetro")
-    print("2 - Área")
-# end pergunta()
-
-
-def quadrado():
+#
+# Metodo para o quadrado, onde calcula e mostra o perimetro ou area.
+#
+def funcQuadrado():
     print("Quadrado")
 # end quadrado()
 
 
-def retangulo():
+#
+# Metodo para o retangulo, onde calcula e mostra o perimetro ou area.
+#
+def funcRetangulo():
     print("Retangulo")
 # end retangulo()
 
 
-def trapezio():
+#
+# Metodo para o trapezio, onde calcula e mostra o perimetro ou area.
+#
+def funcTrapezio():
     print("Trapezio")
 # end trapezio()
 
 
-def circulo():
+#
+# Metodo para o circulo, onde calcula e mostra o perimetro ou area.
+#
+def funcCirculo():
     print("Circulo")
 # end circulo()
 
 
-def losango():
+#
+# Metodo para o losango, onde calcula e mostra o perimetro ou area.
+#
+def funcLosango():
     print("Losango")
 # end losango()
 
 
-def paralelogramo():
+#
+# Metodo para o paralelogramo, onde calcula e mostra o perimetro ou area.
+#
+def funcParalelogramo():
     print("Paralelogramo")
 # end paralelogramo()
 
 
+#
+# Main.
+#
 if __name__ == '__main__':
     # apresentando programa
     apresentacao()
@@ -353,6 +387,7 @@ if __name__ == '__main__':
     # lendo opcao
     opcao = le_opcao()
 
+    # roda ate a opcao ser igual a 0
     while opcao != 0:
         # escolhendo opcao
         escolhe_opcao(opcao)
