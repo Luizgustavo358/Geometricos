ded7934e6a7d6df2ebd1e275288af97e33f5b3ed
Luizgustavo358 <luiz.braganca@sga.pucminas.br>
2018-07-17 15:01:27 -0300

    Função retangulo

--- geometricos.py
+++ geometricos.py
@@ -13,8 +13,8 @@ class Triangulo(object):
 
     #
     # Metodo que calcula o perimetro de um triangulo.
-    # @param int lado1, lado2, base -> passados pelo usuario.
-    # @return int perimetro -> retorna o perimetro.
+    # @param lado1, lado2, base -> passados pelo usuario.
+    # @return perimetro -> retorna o perimetro.
     #
     def perimetro_triangulo(self, lado1, lado2, base):
         # somando lados
@@ -25,8 +25,8 @@ class Triangulo(object):
 
     #
     # Metodo que calcula a area de um triangulo.
-    # @param int base, altura -> passados pelo usuario.
-    # @return int area -> retorna a area.
+    # @param base, altura -> passados pelo usuario.
+    # @return area -> retorna a area.
     #
     def area_triangulo(self, base, altura):
         # calculando a area
@@ -42,7 +42,7 @@ class Quadrado(object):
     #
     # Metodo que calcula o perimetro de um quadrado.
     # @param lado -> passado pelo usuario.
-    # @return int perimetro -> retorna o perimetro.
+    # @return perimetro -> retorna o perimetro.
     #
     def perimetro_quadrado(self, lado):
         # somando lados
@@ -54,7 +54,7 @@ class Quadrado(object):
     #
     # Metodo que calcula a area de um quadrado.
     # @param lado -> passado pelo usuario.
-    # @return int area -> retorna a area.
+    # @return area -> retorna a area.
     #
     def area_quadrado(self, lado):
         # achando a area
@@ -68,7 +68,9 @@ class Quadrado(object):
 class Retangulo(object):
 
     #
-    #
+    # Metodo que calcula o perimetro de um retangulo.
+    # @param base, altura -> passados pelo usuario.
+    # @return perimetro -> retorna o perimetro.
     #
     def perimetro_retangulo(self, base, altura):
         # fazendo a soma dos lados
@@ -78,7 +80,9 @@ class Retangulo(object):
 
 
     #
-    #
+    # Metodo que calcula a area de um retangulo.
+    # @param base, altura -> passados pelo usuario.
+    # @return area -> retorna a area.
     #
     def area_retangulo(self, base, altura):
         # achando a area
@@ -337,7 +341,7 @@ def funcQuadrado():
     print("\n-------------------------------------------------")
     print("\nVocê escolheu: Quadrado")
 
-    # instancia o objeto triangulo
+    # instancia o objeto quadrado
     quadrado = Quadrado()
 
     # mostra as opcoes de calculo
@@ -376,7 +380,43 @@ def funcQuadrado():
 # Metodo para o retangulo, onde calcula e mostra o perimetro ou area.
 #
 def funcRetangulo():
-    print("Retangulo")
+    print("\n-------------------------------------------------")
+    print("\nVocê escolheu: Retangulo")
+
+    # instancia o objeto retangulo
+    retangulo = Retangulo()
+
+    # mostra as opcoes de calculo
+    pergunta()
+
+    # escolhe a opcao
+    tipo = int(input("\nQual tipo deseja: "))
+
+    # testa as opcoes
+    if tipo == 1:
+        # perimetro do quadrado
+        base = le_base()
+        altura = le_altura()
+
+        # calcula o perimetro
+        perimetro = retangulo.perimetro_retangulo(base, altura)
+
+        # mostra o perimetro
+        print("\nO Perímetro do Retangulo é de: ", perimetro)
+        
+    elif tipo == 2:
+        # area do quadrado
+        base = le_base()
+        altura = le_altura()
+
+        # calcula a area
+        area = retangulo.area_retangulo(base, altura)
+
+        # mostra a area
+        print("\nA Área do Retangulo é de: ", area)
+    # end if
+
+    print("\n-------------------------------------------------")
 # end retangulo()
 
 
