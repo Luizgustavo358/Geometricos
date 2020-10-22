ea8c45b68ba37af91cb3a7a1be85816c51e48b24
rafael.machado@solides.com.br <rafael.machado@solides.com.br>
2020-10-21 23:04:29 -0300

    Visualizacao da Diagonal

--- geometricos.py
+++ geometricos.py
@@ -533,8 +533,12 @@ def funcQuadrado():
         # calcula a area
         area = quadrado.area_quadrado(lado)
 
+        # calcula a diagonal
+        diagonal = quadrado.diagonal_quadrado(lado)
+
         # mostra a area
         print("\nA Área do Quadrado é de: ", area)
+        print("\nA Diagonal do Quadrado é de: ", diagonal)
     # end if
 
     print("\n-------------------------------------------------")
